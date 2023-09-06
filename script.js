18d397834d5ee270af0b2c33aa06a6c3dc3fc8dd
“GilsonRavaiani” <gravaiani@yahoo.com.br>
2023-09-06 13:28:53 -0300

    Testando botão retornar em JS

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
+// Seletor para o botão de retorno
+var botaoRetornar = document.getElementById("botao-retornar");
 
-    // Seletor para o botão de retorno
-    var botaoRetornar = document.getElementById("botao-retornar");
-
-    // Adiciona um evento de clique ao botão
-    botaoRetornar.addEventListener("click", function() {
-        // Redireciona o usuário para o index.html
-        window.location.href = "index.html";
-    });
-
-
+// Adiciona um evento de clique ao botão
+botaoRetornar.addEventListener("click", function() {
+    // Redireciona o usuário para o index.html (ou outra página desejada)
+    window.location.href = "index.html";
+});
