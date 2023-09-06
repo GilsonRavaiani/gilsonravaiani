cf8a1c365721afa1b80dc7d10f29da928b27f345
“GilsonRavaiani” <gravaiani@yahoo.com.br>
2023-09-06 12:48:47 -0300

    Iniciando javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+
+    // Seletor para o botão de retorno
+    var botaoRetornar = document.getElementById("botao-retornar");
+
+    // Adiciona um evento de clique ao botão
+    botaoRetornar.addEventListener("click", function() {
+        // Redireciona o usuário para o index.html
+        window.location.href = "index.html";
+    });
+
+
