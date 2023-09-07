02ae88f71a7376da49222573521a67300ddfbc88
“GilsonRavaiani” <gravaiani@yahoo.com.br>
2023-09-06 21:47:35 -0300

    Incluido efeito mãozinha ao passar o mouse botão retornar

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ botaoRetornar.addEventListener("click", function() {
     // Redireciona o usuário para o index.html (ou outra página desejada)
     window.location.href = "index.html";
 });
+
+// Obtém uma referência para o botão
+const botao = document.getElementById("botao-retornar");
+
+// Adiciona um ouvinte de evento de passagem do mouse
+botao.addEventListener("mouseenter", () => {
+    // Altera o estilo do cursor para 'pointer' (mãozinha)
+    botao.style.cursor = "pointer";
+});
+
