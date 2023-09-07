6f2d6bc8c931daa6169930bd12298b5996a210db
“GilsonRavaiani” <gravaiani@yahoo.com.br>
2023-09-07 10:24:48 -0300

    Testando eventos com JS

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-// Seletor para o botão de retorno
 let botaoRetornar = document.getElementById("botao-retornar");
 
-// Adiciona um evento de clique ao botão
 botaoRetornar.addEventListener("click", function() {
-    // Redireciona o usuário para o index.html (ou outra página desejada)
-    window.location.href = "index.html";
+
+alert("Você está sendo redirecionado para página principal")
+
+window.location.href = "index.html";
+
 });
 
-// Obtém uma referência para o botão
 const botao = document.getElementById("botao-retornar");
 
-// Adiciona um ouvinte de evento de passagem do mouse
 botao.addEventListener("mouseenter", () => {
-    // Altera o estilo do cursor para 'pointer' (mãozinha)
+
     botao.style.cursor = "pointer";
 });
 
+let elementoH1 = document.querySelector("h1");
+console.log(elementoH1);
