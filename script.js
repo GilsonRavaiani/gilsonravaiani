81bdb30a9c7fe5d42a3518e915b09faa48f0a948
“GilsonRavaiani” <gravaiani@yahoo.com.br>
2023-09-07 20:34:45 -0300

    Realizado a troca de H1 utilizando apenas JS

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-let botaoRetornar = document.getElementById("botao-retornar");
+//let botaoRetornar = document.getElementById("botao-retornar");
 
-botaoRetornar.addEventListener("click", function() {
+//botaoRetornar.addEventListener("click", function() {
 
-alert("Retornando a página principal")
+//alert("Retornando a página principal")
 
-window.location.href = "index.html";
+//window.location.href = "index.html";
 
-});
+//});
 
-const botao = document.getElementById("botao-retornar");
+//const botao = document.getElementById("botao-retornar");
 
-botao.addEventListener("mouseenter", () => {
+//botao.addEventListener("mouseenter", () => {
 
-    botao.style.cursor = "pointer";
-});
+//    botao.style.cursor = "pointer";
+//});
 
+let elementH1 = document.querySelector("h1")
+
+console.log(elementH1)
+
+//main.appendChild(elementH1)
+
+elementH1.innerText = `Gilson Ravaiani` 
