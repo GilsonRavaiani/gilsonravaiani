00999eda047bbbe90e3e2c086576a0268af5adb6
“GilsonRavaiani” <gravaiani@yahoo.com.br>
2023-09-08 12:11:08 -0300

    Trabalhando tipos de botões

--- script.js
+++ script.js
@@ -8,16 +8,16 @@ let botaoRetornar = document.getElementById("botao-retornar");
 
 botaoRetornar.addEventListener("click", function() {
 
+
 alert("Retornando a página principal")
 
 window.location.href = "index.html";
 
 });
 
-const botao = document.getElementById("botao-retornar");
+let botao = document.getElementById("botao-retornar");
 
 botao.addEventListener("mouseenter", () => {
 
     botao.style.cursor = "pointer";
 });
-
